2755aa5a9fa30b68717a61523f13883fbe500dcc
Full Name <nickfuentes24@gmail.com>
2019-06-29 17:48:34 -0500

    Created a new function that will take a number to check and a number to divide from the user.

--- odd-or-even.py
+++ odd-or-even.py
@@ -6,14 +6,28 @@ else:
     print("You number is ODD!")"""
 
 
-def odd_even():
-    user_input = int(input("Enter a number: "))
-    if user_input % 4 == 0:
+"""def odd_even():
+    num_check = int(input("Enter a number to check: "))
+    if num_check % 4 == 0:
         print("Your number is a Mutilple of 4 and EVEN!")
-    elif user_input % 2 == 0:
+    elif num_check % 2 == 0:
         print("Your number is EVEN!")
     else:
         print("Your number is ODD!")
 
 
-odd_even()
+odd_even()"""
+
+
+def num_divide():
+    num_check = int(input("Enter a number to check: "))
+    num_divide = int(input("Enter a number to divide: "))
+    if num_check % 4 == 0:
+        print("Your number is a Mutilple of 4 and divides evenly!")
+    elif num_check % num_divide == 0:
+        print("Your number diveds evenly!")
+    else:
+        print("Your number does not divide evenly!")
+
+
+num_divide()
