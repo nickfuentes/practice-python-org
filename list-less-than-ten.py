b0d7a7f48a18d59bd7e3f24d779cd6ad8754177b
Full Name <nickfuentes24@gmail.com>
2019-06-29 18:06:07 -0500

    Turned the code in a function so you can just call the function.

--- list-less-than-ten.py
+++ list-less-than-ten.py
@@ -1,7 +1,17 @@
 array = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
+"""
 for num in range(0, len(array), 1):
     if array[num] < 5:
-        print(array[num])
+        print(array[num])"""
 
-    # print(array[num]) print out numbers in arrary.
+# print(array[num]) print out numbers in arrary.
+
+
+def less_than_5():
+    for num in range(0, len(array), 1):
+        if array[num] < 5:
+            print(array[num])
+
+
+less_than_5()
