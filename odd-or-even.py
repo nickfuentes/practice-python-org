8571685a839e5417d8ed310fe06759d57d9e2bb6
Full Name <nickfuentes24@gmail.com>
2019-06-29 17:34:42 -0500

    Added a condition if the number entered iis a multiple of 4 it will print out multiple of 4 and even message.

--- odd-or-even.py
+++ odd-or-even.py
@@ -8,7 +8,9 @@ else:
 
 def odd_even():
     user_input = int(input("Enter a number: "))
-    if user_input % 2 == 0:
+    if user_input % 4 == 0:
+        print("Your number is a Mutilple of 4 and EVEN!")
+    elif user_input % 2 == 0:
         print("Your number is EVEN!")
     else:
         print("Your number is ODD!")
