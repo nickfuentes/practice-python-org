3741643900ff9b48b87ded7ff0875f68774b5a65
Full Name <nickfuentes24@gmail.com>
2019-06-29 18:01:19 -0500

    Wrote a program that loops throug an array and prints out all numbers that are less than 5.

---
+++ list-less-than-ten.py
@@ -0,0 +1,7 @@
+array = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+for num in range(0, len(array), 1):
+    if array[num] < 5:
+        print(array[num])
+
+    # print(array[num]) print out numbers in arrary.
