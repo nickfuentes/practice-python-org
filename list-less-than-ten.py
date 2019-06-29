7f0afa3440b2723b5ecfe2a4380fa8b8d304a9b4
Full Name <nickfuentes24@gmail.com>
2019-06-29 18:15:59 -0500

    Wrote the function to append the numbers less than 5 into a new array and print it out.

--- list-less-than-ten.py
+++ list-less-than-ten.py
@@ -1,4 +1,5 @@
 array = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+new_array = []
 
 """
 for num in range(0, len(array), 1):
@@ -8,10 +9,26 @@ for num in range(0, len(array), 1):
 # print(array[num]) print out numbers in arrary.
 
 
-def less_than_5():
+"""def less_than_5():
     for num in range(0, len(array), 1):
         if array[num] < 5:
             print(array[num])
 
 
+less_than_5()"""
+
+
+def less_than_5():
+    for num in range(0, len(array), 1):
+        if array[num] < 5:
+            new_array.append(array[num])
+
+
+# def print_new_array():
+    for num in range(0, len(new_array), 1):
+        print(new_array[num])
+
+
 less_than_5()
+print(new_array)
+# print_new_array()
